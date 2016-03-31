2cb4791cfa4f0587a3f7e1074925e778640c6a50
Aurelien Martin <01aurelien@gmail.com>
2016-03-31 10:22:24 +0200

    New packages

--- configs/debian/debian-mypkg.sh
+++ configs/debian/debian-mypkg.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-pkg_global='i3 tmux rxvt-unicode terminator mc ranger aspell-fr aspell-en sshfs qpdfview unrar ccze rsync unison unison-gtk apcalc arandr encfs taskwarrior rxvt-unicode roxterm terminator' 
+pkg_global='i3 tmux rxvt-unicode terminator mc ranger aspell-fr aspell-en sshfs qpdfview unrar ccze rsync unison unison-gtk apcalc arandr encfs taskwarrior rxvt-unicode roxterm terminator fdupes'
 pkg_system='aptitude iftop alien rkhunter htop lshw bash-completion pv powerstat wicd-curses ftp apt-file lsscsi iotop'
 pkg_devel='vim git tig gdb ddd pylint python-pip exuberant-ctags gcc g++ build-essential autoconf automake autotools-dev fakeroot libncurses5-dev ncurses-base pgadmin3 virtualenv cmake puppet-lint python-pylint-django'
-pkg_debian='apt-listbugs apt-listchanges debhelper dh-make debmake devscripts lintian quilt checkinstall'
+pkg_debian='apt-listbugs apt-listchanges debhelper dh-make debmake devscripts lintian quilt checkinstall pbuilder'
 pkg_elec='arduino oregano gspiceui'
 pkg_internet='tor hexchat irssi w3m nmap chromium icedove ncftp'
 pkg_sound='alsa-base alsa-utils alsa-oss alsamixergui libasound2 libasound2-doc pulseaudio pavucontrol'
@@ -11,8 +11,8 @@ pkg_desktop='xinit iceweasel pcmanfm gpicview systemd-gui '
 pkg_multimedia='vlc cdparanoia ripperx flac libdvdread4 darktable mesa-utils'
 pkg_recording='jackd qjackctl multimedia-firewire ardour calf-plugins hydrogen hydrogen-drumkits hydrogen-drumkits-effects zynaddsubfx x42-plugins qsynth zam-plugins vlc-plugin-jack'
 pkg_nonfree='flashplugin-nonfree'
-pkg_office='krb5-config krb5-doc krb5-user'
-pkg_security='keepassx kpcli crunch wireshark tcpdump'
+pkg_office='krb5-config krb5-doc krb5-user aspell-fr aspell-en hunspell-en-us hunspell-fr'
+pkg_security='keepassx pass kpcli crunch wireshark tcpdump pwgen'
 pkg_goodies='fonts-inconsolata'
 pkg_network='whois dnsutils'
 
