9ffc1abe1c468aaf857804b7fc64f5a1d490e0bb
Aurelien Martin <01aurelien@gmail.com>
2016-10-05 10:18:16 +0000

    Split functions in deploydot.sh

--- deploydot.sh
+++ deploydot.sh
@@ -8,37 +8,51 @@
 OS=`uname`
 ROOT_OF_REPO=$PWD
 
-info(){
+globalvar(){
     echo "OS: $OS"
     echo "User: $USER"
     echo "Home: $HOME"
     echo "Repository Path $ROOT_OF_REPO" 
 }
 
-getDotFilesNamePerOSInRepo(){
-    find ./dotfiles/common -maxdepth 2  -type f | cut -sd / -f 3-
-    find ./dotfiles/$1 -maxdepth 2 -type f | cut -sd / -f 3-
+getFileToLink(){
+    find ./dotfiles/common -maxdepth 1  -type f | cut -sd / -f 3-
+    find ./dotfiles/$1 -maxdepth 1 -type f | cut -sd / -f 3-
 }
 
-DotFilesPerOSInRepo=$(getDotFilesNamePerOSInRepo $OS)
+getDirectoryToLink(){
+    find ./dotfiles/common/ -mindepth 1 -maxdepth 1  -type d | cut -sd / -f 3-
+    find ./dotfiles/$1 -mindepth 1 -maxdepth 1  -type d | cut -sd / -f 3-
+}
 
-for i in $DotFilesPerOSInRepo; do
-  #Get the basename of the file
-  filename=`echo $i | cut -sd / -f 2-`
-   #If the config file exist, backup and create a symblink to the repo
-   if [ -f ~/\.$filename ] ; then
-      #If the config file is a symblink do nothing (for the moment)
-      if [ -L ~/\.$filename ] ; then
-         echo "$filename is symblink do nothing"
+CreateLink(){
+
+  for file in $@; do
+      #Get the basename of the file
+      filename=`echo $file | cut -sd / -f 2-`
+      #If the config file exist, backup and create a symblink to the repo
+      if [ -f ~/\.$filename ] || [ -d ~/\.$filename ]; then
+          #If the config file is a symblink do nothing (for the moment)
+          if [ -L ~/\.$filename ] ; then
+          target=`readlink -f $filename` 
+          echo "$filename is a symblink -> $target"
       else
+         #Backup the existing file and create the symb link
          echo ".$filename is a regular file in $HOME"
-	 echo "Move ~/.$filename to ~/.$filename.orig"
+	     echo "Move ~/.$filename to ~/.$filename.orig"
          mv ~/.$filename ~/.$filename.orig
          echo "Symblink $ROOT_OF_REPO/dotfiles/$i to ~/.$filename"
          ln -s $ROOT_OF_REPO/dotfiles/$i ~/.$filename
       fi
-   else
-      echo ".$filename doesn't exist in $HOME. Symblink $ROOT_OF_REPO/dotfiles/$i to ~/.$filename"
-      ln -s $ROOT_OF_REPO/dotfiles/$i ~/.$filename
+  else
+      echo ".$filename doesn't exist in $HOME. Symblink $ROOT_OF_REPO/dotfiles/$file to ~/.$filename"
+      ln -s $ROOT_OF_REPO/dotfiles/$file ~/.$filename
    fi
 done
+
+}
+
+FileToLink=$(getFileToLink $OS)
+DirectoryToLink=$(getDirectoryToLink $OS)
+
+CreateLink $DirectoryToLink $FileToLink
